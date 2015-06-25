f65cf3fc94998d9427cdff24a6aa236414da9fd6
Steve Goldman <steve@wespeaktogether.com>
2015-06-25 11:59:43 -0400

    stubbing out the other controller impls

---
+++ PunchOutTabs/CardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CardViewController.swift
+//  PunchOutTabs
+//
+//  Created by Steve Goldman on 6/25/15.
+//  Copyright (c) 2015 Steve Goldman. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+}
