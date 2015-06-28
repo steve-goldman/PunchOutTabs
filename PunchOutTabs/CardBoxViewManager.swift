69371de6049ac3f2ad63f755dfcc88db4dd3fa05
Steve Goldman <steve@wespeaktogether.com>
2015-06-27 23:24:23 -0400

    generalized the card box view manager for MxN boxes

--- PunchOutTabs/CardBoxViewManager.swift
+++ PunchOutTabs/CardBoxViewManager.swift
@@ -51,23 +51,48 @@ class CardBoxContainerView: UIView {
     }
 }
 
-class TwoByTwoCardBoxViewManager: CardBoxContainerView
+class MbyNCardBoxViewManager: CardBoxContainerView
 {
-    static let NumBoxes = 4
-    
+    private let m: Int
+    private let n: Int
     private let vertBuffer: CGFloat
     private let horizBuffer: CGFloat
     private let forceSquare: Bool
     
-    init(parentView: UIView, vertBuffer: CGFloat = 0.05, horizBuffer: CGFloat = 0.05, forceSquare: Bool = false, initializer: () -> UIView) {
+    init(m: Int, n: Int, parentView: UIView, vertBuffer: CGFloat = 0.05, horizBuffer: CGFloat = 0.05, forceSquare: Bool = false, initializer: () -> UIView) {
+        self.m = m
+        self.n = n
         self.vertBuffer = vertBuffer
         self.horizBuffer = horizBuffer
         self.forceSquare = forceSquare
-        super.init(parentView: parentView, viewCount: TwoByTwoCardBoxViewManager.NumBoxes, initializer: initializer)
+        super.init(parentView: parentView, viewCount: m * n, initializer: initializer)
     }
     
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private static func startingOffset(#dim: Int, frameDim: CGFloat, sizeDim: CGFloat, bufferDim: CGFloat) -> CGFloat {
+        var offset = (frameDim / 2)
+        if dim > 2 {
+            for _ in 1...((dim - 1) / 2) {
+                offset = offset.advancedBy(-bufferDim)
+            }
+        }
+        if dim % 2 == 0 {
+            offset = offset.advancedBy(-bufferDim / 2)
+        }
+        
+        if dim > 1 {
+            for _ in 1...(dim / 2) {
+                offset = offset.advancedBy(-sizeDim)
+            }
+        }
+        if dim % 2 == 1 {
+            offset = offset.advancedBy(-sizeDim / 2)
+        }
+
+        return offset
     }
 
     override func recomputeDeltasAndSize() {
@@ -75,19 +100,30 @@ class TwoByTwoCardBoxViewManager: CardBoxContainerView
         
         let buffer = CGSize(width: horizBuffer * frame.width, height: vertBuffer * frame.height)
         
-        let width = (frame.width - 3 * buffer.width) / 2
-        let height = (frame.height - 3 * buffer.height) / 2
+        let width = (frame.width - CGFloat(m + 1) * buffer.width) / CGFloat(m)
+        let height = (frame.height - CGFloat(n + 1) * buffer.height) / CGFloat(n)
         let minDim = min(width, height)
         
         size = forceSquare ? CGSize(width: minDim, height: minDim) : CGSize(width: width, height: height)
         
         // working clockwise
-        deltas = [
-            CGPoint(x: frame.width / 2 - buffer.width / 2 - size.width, y: frame.height / 2 - buffer.height / 2 - size.height),
-            CGPoint(x: size.width + buffer.width, y: 0),
-            CGPoint(x: 0, y: size.height + buffer.height),
-            CGPoint(x: -size.width - buffer.width, y: 0)
-        ]
+        deltas = [CGPoint]()
+        
+        deltas.append(CGPoint(x: MbyNCardBoxViewManager.startingOffset(dim: m, frameDim: frame.width, sizeDim: size.width, bufferDim: buffer.width), y: MbyNCardBoxViewManager.startingOffset(dim: n, frameDim: frame.height, sizeDim: size.height, bufferDim: buffer.height)))
+        
+        // by column
+        for column in 1...n {
+            // by row
+            if m > 1 {
+                for _ in 1...(m - 1) {
+                    deltas.append(CGPoint(x: (column % 2 == 1 ? 1 : -1) * (size.width + buffer.width), y: 0))
+                }
+            }
+            if column != n {
+                deltas.append(CGPoint(x: 0, y: size.height + buffer.height))
+            }
+        }
+
     }
     
 }
@@ -112,8 +148,8 @@ class TwoTwoByTwoCardBoxViewManager: MultipleCardBoxContainerViewManager
     
     init(parentView: UIView, vertBuffer: CGFloat = 0.05, horizBuffer: CGFloat = 0.05, forceSquare: Bool = false, initializer: () -> UIView) {
         super.init(cardBoxContainers: [
-            TwoByTwoCardBoxViewManager(parentView: parentView, vertBuffer: vertBuffer, horizBuffer: horizBuffer, forceSquare: forceSquare, initializer: initializer),
-            TwoByTwoCardBoxViewManager(parentView: parentView, vertBuffer: vertBuffer, horizBuffer: horizBuffer, forceSquare: forceSquare, initializer: initializer) ])
+            MbyNCardBoxViewManager(m: 2, n: 2, parentView: parentView, vertBuffer: vertBuffer, horizBuffer: horizBuffer, forceSquare: forceSquare, initializer: initializer),
+            MbyNCardBoxViewManager(m: 2, n: 2, parentView: parentView, vertBuffer: vertBuffer, horizBuffer: horizBuffer, forceSquare: forceSquare, initializer: initializer) ])
     }
 
 }
