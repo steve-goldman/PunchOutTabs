34f4dd1741641dfafe7e08bb5ce970837ce9e4f2
Steve Goldman <steve@wespeaktogether.com>
2015-07-06 10:46:44 -0400

    tweak

---
+++ PunchOutTabs/CoreExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  CoreExtensions.swift
+//  PunchOutTabs
+//
+//  Created by Steve Goldman on 7/6/15.
+//  Copyright (c) 2015 Steve Goldman. All rights reserved.
+//
+
+import UIKit
+
+class CoreExtensions: NSObject {
+   
+}
