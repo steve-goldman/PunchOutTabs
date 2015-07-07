2a570cb8e23f05e6f0b0098445fc6e614bd7aef9
Steve Goldman <steve@wespeaktogether.com>
2015-07-07 18:20:32 -0400

    starting to use cloud code for new card flow

---
+++ CloudCode/cloud/new_card.js
@@ -0,0 +1,78 @@
+//
+// methods called by users via cloud definitions in main.js
+//
+
+// user has pressed "next" after setting the name in the new card flow
+exports.nameOfNewCardPressed = function(request, response) {
+    // validate the name
+    var result = validateName(request.params.name);
+    if (result != null) {
+        response.error(result);
+        return;
+    }
+
+    // create a new card template and save it
+    var CardTemplate = Parse.Object.extend("CardTemplate");
+    var cardTemplate = new CardTemplate();
+    cardTemplate.save({
+        name: request.params.name,
+        isActive: false
+    }, {
+        success: function(cardTemplate) {
+            // if the current user has a pending card, delete it
+            var pendingNewCard = Parse.User.current().get("pendingNewCard");
+            if (pendingNewCard != null) {
+                console.log("attempting to remove old card");
+                pendingNewCard.destroy({
+                    success: function(pendingNewCard) {
+                        associateCardTemplateWithUser(cardTemplate, response);
+                    },
+                    error: function(pendingNewCard, error) {
+                        // not fatal
+                        console.log("Could not destroy old card due to: " + error);
+                        associateCardTemplateWithUser(cardTemplate, response);
+                    }
+                });
+            } else {
+                console.log("no old card to remove");
+                associateCardTemplateWithUser(cardTemplate, response);
+            }
+        },
+        error: function(cardTemplate, error) {
+            response.error("Could not save card template due to: " + error);
+        }
+    });
+}
+
+// helper method to previous
+associateCardTemplateWithUser = function(cardTemplate, response) {
+    Parse.User.current().set("pendingNewCard", cardTemplate);
+    Parse.User.current().save(null, {
+        success: function(user) {
+            response.success(true);
+        },
+        error: function(user, error) {
+            response.error("Could not associate card template with user due to: " + error);
+        }
+    });
+}
+
+// user has pressed "next" after setting the end date in the new card flow
+exports.endDateOfNewCardPressed = function(request, response) {
+}
+
+//
+// private section
+//
+
+// validate the name of a card
+validateName = function(name) {
+    const NAME_MIN_LENGTH = 4;
+    const NAME_MAX_LENGTH = 32;
+
+    if (name.length < NAME_MIN_LENGTH || name.length > NAME_MAX_LENGTH) {
+        return "Name must be between " + NAME_MIN_LENGTH + " and " + NAME_MAX_LENGTH + " characters";
+    }
+
+    return null;
+}
--- PunchOutTabs/NameOfNewCardViewController.swift
+++ PunchOutTabs/NameOfNewCardViewController.swift
@@ -38,38 +38,14 @@ class NameOfNewCardViewController: UIViewController, UITextFieldDelegate
     // MARK: - Actions
     
     @IBAction func nextPressed() {
-        let name = cardNameField.text
-        
-        let result = CardTemplateValidator.validate(name: name)
-        if result.valid {
-            // create a new card template and save it
-            let cardTemplate = CardTemplate.create(name)
-            activityIndicator.startAnimating()
-            cardTemplate.saveInBackgroundWithBlock { (success, error) in
-                self.activityIndicator.stopAnimating()
-                if success {
-                    // remove the previous card the user may have been working on
-                    if PFUser.currentUser()!.pendingNewCard != nil {
-                        PFUser.currentUser()!.pendingNewCard?.deleteInBackground()
-                    }
-                    
-                    // associate the card with the user and save the user
-                    PFUser.currentUser()!.pendingNewCard = cardTemplate
-                    self.activityIndicator.startAnimating()
-                    PFUser.currentUser()!.saveInBackgroundWithBlock { (success, error) in
-                        self.activityIndicator.stopAnimating()
-                        if success {
-                            self.performSegueWithIdentifier(SegueIdentifier.Next, sender: nil)
-                        } else {
-                            UIAlertView(title: "Could not set name", message: "Something went wrong: \(error!.localizedDescription)", delegate: nil, cancelButtonTitle: "Got it").show()
-                        }
-                    }
-                } else {
-                    UIAlertView(title: "Could not set name", message: "Something went wrong: \(error!.localizedDescription)", delegate: nil, cancelButtonTitle: "Got it").show()
-                }
+        activityIndicator.startAnimating()
+        PFCloud.callFunctionInBackground("nameOfNewCardNextPressed", withParameters: ["name": cardNameField.text]) { (result, error) in
+            self.activityIndicator.stopAnimating()
+            if result != nil {
+                self.performSegueWithIdentifier(SegueIdentifier.Next, sender: nil)
+            } else {
+                UIAlertView(title: "Oops...", message: error!.localizedDescription, delegate: nil, cancelButtonTitle: "Got it").show()
             }
-        } else {
-            UIAlertView(title: result.errorSection, message: result.errorMessage, delegate: nil, cancelButtonTitle: "Got it").show()
         }
     }
     
